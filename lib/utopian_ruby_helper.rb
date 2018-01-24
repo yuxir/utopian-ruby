8fa21ec9e5be9786dc8a2cd2f3ed546bf0359914
Yuxi <me@yuxifan.com>
2018-01-24 22:44:11 +0000

    add get_posts_obj_by_type and rspec test

--- lib/utopian_ruby_helper.rb
+++ lib/utopian_ruby_helper.rb
@@ -10,6 +10,15 @@ module UtopianRuby
       params["type"]=type
       UtopianRuby::UtopianRubyAPI.get_posts(params)
     end
+
+    # get posts object by type
+    def self.get_posts_obj_by_type(type,params)
+      if params.nil?
+         params = {"limit":1}
+      end
+      params["type"]=type
+      UtopianRuby::UtopianRubyAPI.get_posts_obj(params)
+    end
   end
 end
 
--- spec/utopian_ruby_helper_spec.rb
+++ spec/utopian_ruby_helper_spec.rb
@@ -5,4 +5,13 @@ describe UtopianRuby::UtopianRubyHelper do
   it "has get_posts_by_type" do
     expect(UtopianRuby::UtopianRubyHelper.get_posts_by_type("development",{"limit":5})["results"].length).to eq(5)
   end
+
+  it "has get_posts_obj_by_type" do
+    expect(UtopianRuby::UtopianRubyHelper.get_posts_obj_by_type("development",{"limit":5}).length).to eq(5)
+    #UtopianRuby::UtopianRubyHelper.get_posts_obj_by_type("development",{"limit":5}).each do |p|
+    #  puts p.author
+    #  puts p.title
+    #  puts p.permlink
+    #end
+  end
 end
